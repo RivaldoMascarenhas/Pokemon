fce3f1a57be9072a90f66a7922240dc2f4d3d8e2
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-07-11 18:15:53 -0300

    delete import

--- src/Global/styledProvider.tsx
+++ src/Global/styledProvider.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../theme/_defaultTheme";
 import { GlobalStyled } from "./globalStyled";
-import { useGetApi } from "@/hooks";
 
 export default function StyledProvider({ children }: { children: ReactNode }) {
   return (
